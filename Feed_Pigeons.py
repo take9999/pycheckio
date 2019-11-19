1aa8e5bc8e4501a6cdd7e9e9c20fee436b0f787d
tnishida <tnishida0309@gmail.com>
2019-11-19 19:20:58 +0900

    first commit

---
+++ Feed_Pigeons.py
@@ -0,0 +1,14 @@
+def checkio(number):
+    # N分後にいる鳩の数
+    # N分後に必要な鳩の餌の数
+
+    return 0
+
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio(1) == 1, "1st example"
+    assert checkio(2) == 1, "2nd example"
+    assert checkio(5) == 3, "3rd example"
+    assert checkio(10) == 6, "4th example"
+
