f089cc8a46f0f8c60d57b63b4527d615ea487481
tnishida <tnishida0309@gmail.com>
2019-11-23 21:22:34 +0900

    first commit

---
+++ The_Flat_Dictionary.py
@@ -0,0 +1,43 @@
+def flatten(dictionary):
+    sep = "/"
+
+    def recurse_dict(t, parent_key=""):
+        if isinstance(t, dict):
+            for k, v in t.items():
+                if isinstance(v, dict):
+                    recurse_dict(v, parent_key + sep + k)
+                else:
+                    return {parent_key + sep + k: v}
+        else:
+            print("not dict")
+
+    return recurse_dict(dictionary)
+
+
+if __name__ == '__main__':
+    test_input = {"key": {"deeper": {"more": {"enough": "value"}}}}
+    print(' Input: {}'.format(test_input))
+    print('Output: {}'.format(flatten(test_input)))
+
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert flatten({"key": "value"}) == {"key": "value"}, "Simple"
+    assert flatten(
+        {"key": {"deeper": {"more": {"enough": "value"}}}}
+    ) == {"key/deeper/more/enough": "value"}, "Nested"
+    assert flatten({"empty": {}}) == {"empty": ""}, "Empty value"
+    assert flatten({"name": {
+        "first": "One",
+        "last": "Drone"},
+        "job": "scout",
+        "recent": {},
+        "additional": {
+            "place": {
+                "zone": "1",
+                "cell": "2"}}}
+    ) == {"name/first": "One",
+          "name/last": "Drone",
+          "job": "scout",
+          "recent": "",
+          "additional/place/zone": "1",
+          "additional/place/cell": "2"}
+    print('You all set. Click "Check" now!')
