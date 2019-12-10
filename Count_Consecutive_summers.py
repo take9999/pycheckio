7173eb45043ee6fa6cc2d0758a0c01ee94495b17
tnishida <tnishida0309@gmail.com>
2019-12-10 21:33:52 +0900

    first commit

---
+++ Count_Consecutive_summers.py
@@ -0,0 +1,14 @@
+def count_consecutive_summers(num):
+
+    return None
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(count_consecutive_summers(42))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert count_consecutive_summers(42) == 4
+    assert count_consecutive_summers(99) == 6
+    assert count_consecutive_summers(1) == 1
+    print("Coding complete? Click 'Check' to earn cool rewards!")
